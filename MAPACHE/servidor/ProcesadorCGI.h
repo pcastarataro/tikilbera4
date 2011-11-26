38b6050b30356ee2d43081c0c6fb235f8022a133
seiya.gustavo@gmail.com <seiya.gustavo@gmail.com@dc9eca02-32d0-3ea1-a099-dc461f6b09f2>
2011-11-26 23:44:26 +0000

    comentado para doxy

--- MAPACHE/servidor/ProcesadorCGI.h
+++ MAPACHE/servidor/ProcesadorCGI.h
@@ -24,22 +24,79 @@ using std::string;
 
 class ProcesadorCGI: public IProcesador {
 private:
-	std::string dir;
-	std::string rutaCGI;
+	string dir;
+	string rutaCGI;
 	const Configuracion& config;
 
+	/**
+	 * Crea una nueva instancia de HTTP_Response y setea su contenido con 
+	 * el archivo que tiene como ruta a rutaCompletaArchSalida.
+	 * @param rutaCompletaArchSalida
+	 * @return HTTP_Response*
+	 */
 	HTTP_Response* setResponse(const char* rutaCompletaArchSalida);
-	void removerArchivosYDirectoriosTemporales(const string rutaArchEntrada, const string rutaArchSalida, 
-			const string directorioCGI);
-	string crearDirectorioTemporalParaElCGI(int* nroCarpeta);
+	/**
+	 * Elimina los archivos y/o directorios creados por el CGI, luego de finalizada su ejecucion.
+	 * @param rutaArchEntrada Ruta del archivo que el CGI toma como entrada.
+	 * @param rutaArchSalida Ruta del archivo que el CGI genero como salida.
+	 * @param directorioCGI Ruta del directorio en donde el CGI almaceno sus archivos.
+	 * @return void
+	 */
+	void removerArchivosYDirectoriosTemporales(const string rutaArchEntrada, 
+			const string rutaArchSalida, const string directorioCGI);
+	/**
+	 * Crea el archivo que el CGI tomara como entrada y lo carga con la cadena contenidoDeRequest.
+	 * @param rutaCompletaArchEntradaTemp Ruta del archivo que el CGI toma como entrada.
+	 * @param contenidoDeRequest Basicamente es el POST del request.
+	 * @return void
+	 */
 	void crearArchivoTemporalParaSTDIN(const string& rutaCompletaArchEntradaTemp, const string& contenidoDeRequest);
-	void prepararComandoDeTerminal(const string rutaCGI, const string& uri, string& comando, const string& nombreEntradaTemp, 
-			const string& nombreSalidaTemp, const string& dirArchivosGeneradosPorCGI);
+	/**
+	 * Genera el comando para terminal que se ejecutara para el CGI, segun los parametros pasados.
+	 * @param rutaCGI Ruta relativa (del cgi) a la carpeta cgi-bin.
+	 * @param uri
+	 * @param nombreEntradaTemp Ruta del archivo que el CGI toma como entrada.
+	 * @param nombreSalidaTemp Ruta del archivo que el CGI genera como salida.
+	 * @param dirArchivosGeneradosPorCGI Ruta del directorio en donde el CGI almaceno sus archivos.
+	 * @return void
+	 */
+	void prepararComandoDeTerminal(const string rutaCGI, const string& uri, 
+			string& comando, const string& nombreEntradaTemp, 
+			const string& nombreSalidaTemp, 
+			const string& dirArchivosGeneradosPorCGI);
+	/**
+	 * Setea el valor de la variable de entorno content_length.
+	 * @param valor
+	 * @return void
+	 */
 	void setVariableContentLenght(int valor);
 public:
+	/**
+	 * Constructor
+	 */
 	ProcesadorCGI(const Configuracion& config, const std::string& ruta , const std::string& rutaCgi);
+	/**
+	 * Destructor
+	 */
 	virtual ~ProcesadorCGI();
-	HTTP_Response* procesarCGI(HTTP_Request* request, string rutaCGI, const string& pathAbsoluto, string dirArchivosDeSalida);
+	/* Prepara al sistema y al CGI para su correcta ejecucion y luego lo ejecuta.
+	 * Controla que el CGI exista y borra los archivos temporales que este cree.
+	 * Se agrega al log la ejecucion del CGI en cuestion.
+	 * @param request.
+	 * @param rutaCGI Ruta relativa (del cgi) a la carpeta cgi-bin.
+	 * @param pathAbsolutoDelRecurso.
+	 * @param dirArchivosDeSalida Ruta absoluta del directorio en 
+	 * donde el CGI almacenara los archivos que genere.
+	 * @return HTTP_Response* Una nueva instancia de HTTP_Response, 
+	 * seteada con la salida generada por el CGI.
+	 *  */
+	HTTP_Response* procesarCGI(HTTP_Request* request, string rutaCGI, 
+			const string& pathAbsolutoDelRecurso, string dirArchivosDeSalida);
+	/**
+	 * Metodo que implementa de IProcesador.
+	 * @param pedido es el pedido a procesar
+	 * @return es la respuesta generada por el pedido
+	 */
 	HTTP_Response* procesar(HTTP_Request* pedido);
 };
 
