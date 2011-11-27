5c4e7771efb620587ebaccb43ee623fd6edff205
edgardo.skl@gmail.com <edgardo.skl@gmail.com@dc9eca02-32d0-3ea1-a099-dc461f6b09f2>
2011-11-27 15:06:17 +0000

    modifico el clean.sh

--- trunk/MAPACHE/clean.sh
+++ trunk/MAPACHE/clean.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
-cd build/
-$prefix_client make clean
-cd ../ 
+rm -f configPorDefecto.xml
+
+if [ -d "build/" ]
+then
+	echo "Borrando archivos de la aplicacion..."
+	cd build/
+	$prefix_client make clean
+	cd ../ 
+	rm -r -f build
+fi
+
+if [ -d "cgi-bin" ]
+then
 rm -r -f cgi-bin
+fi
+
+if [ -d "arch-temp-cgi" ]
+then
 rm -r -f arch-temp-cgi
-rm -r -f build
-rm -r -f Documentacion/Doxy
+fi
+
+if [ -d "build/" ]
+then
+	echo "Borrando documentacion..."
+	rm -r -f Documentacion/Doxy/
+fi
+
 #mkdir -p $(BASEDIR)
 #cp -r -f ../$(BASEDIR) ./
