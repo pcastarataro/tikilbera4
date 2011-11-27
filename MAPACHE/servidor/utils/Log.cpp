067acbd727520ad95adfd84a266dda76efd6f7b9
pablocastarataro@gmail.com <pablocastarataro@gmail.com@dc9eca02-32d0-3ea1-a099-dc461f6b09f2>
2011-11-27 00:39:57 +0000

    Arreglo sigsev del manejador de logs

--- MAPACHE/servidor/utils/Log.cpp
+++ MAPACHE/servidor/utils/Log.cpp
@@ -7,28 +7,10 @@ Log::Log(const std::string& pathLogFile) :
 	layoutPattern = new log4cpp::PatternLayout();
 	layoutPattern->setConversionPattern(format);
 	appender->setLayout(layoutPattern);
-//	layout = new log4cpp::SimpleLayout();
-//	appender->setLayout(layout);
 	category.setAppender(appender);
 	category.setPriority(log4cpp::Priority::INFO);
 }
 
-//Sets the format of log lines handled by this PatternLayout.
-//
-//By default, set to "%m%n".
-//Format characters are as follows:
-//%%%% - a single percent sign
-//%c - the category
-//%d - the date
-//
-//Date format: The date format character may be followed by a date format specifier enclosed between braces. For example, %d{%H:%M:%S,%l} or %d{%d %m %Y %H:%M:%S,%l}. If no date format specifier is given then the following format is used: "Wed Jan 02 02:03:55 1980". The date format specifier admits the same syntax as the ANSI C function strftime, with 1 addition. The addition is the specifier %l for milliseconds, padded with zeros to make 3 digits.
-//%m - the message
-//%n - the platform specific line separator
-//%p - the priority
-//%r - milliseconds since this layout was created.
-//%R - seconds since Jan 1, 1970
-//%u - clock ticks since process start
-//%x - the NDC
 Log::Log(const std::string& pathLogFile, const std::string& format) :
 	category(log4cpp::Category::getInstance(pathLogFile)) {
 	appender = new log4cpp::FileAppender("FileAppender", pathLogFile);
@@ -40,7 +22,6 @@ Log::Log(const std::string& pathLogFile, const std::string& format) :
 }
 
 Log::~Log() {
-	log4cpp::Category::shutdown();
 }
 
 void Log::logWarning(const std::string& menssage) {
